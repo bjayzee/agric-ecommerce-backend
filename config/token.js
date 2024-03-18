10ee102fab04dfbfd3e6156c69e8e33e66da570e
bjayzee <bjayzee@gmail.com>
2024-03-18 06:03:46 +0100

    project update

--- config/token.js
+++ config/token.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 
 const encryptPassword = async(password) =>{
-    const salt = await bcrypt.genSalt();
+    const salt = await bcrypt.genSalt(8);
     return bcrypt.hashSync(password, salt);
 }
 
